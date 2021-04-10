fa9ce4364ba744275f93ad540339d3816dff220e
Hassen Hannachi <hannachihassen07@gmail.com>
2021-04-09 23:37:21 -0400

    Add Select class

--- src/main/java/com/BasicSelinum/AccessDropDown.java
+++ src/main/java/com/BasicSelinum/AccessDropDown.java
@@ -6,11 +6,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class AccessDropDown {
+	public static WebDriver driver=null;
+	
 	public static void main(String[] args) { 
 		System.setProperty("webdriver.gecko.driver",
 				"C:\\Users\\HASSEN\\workspace\\BasicSelinum_Tutorail_Suite\\Drivers\\geckodriver.exe");
 	    String baseURL = "http://demo.guru99.com/test/newtours/register.php";
-	    WebDriver driver = new FirefoxDriver();
+	    driver = new FirefoxDriver();
 		driver.get(baseURL);
 
 		Select drpCountry = new Select(driver.findElement(By.name("country")));
